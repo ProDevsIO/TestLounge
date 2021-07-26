26f04b7181f888ffac91ff4546a69b76e0fade80
williamnwogbo <williamnwogbo@gmail.com>
2021-07-26 15:56:39 +0100

    Validation check

--- config/mail.php
+++ config/mail.php
@@ -15,6 +15,7 @@ return [
 
     'default' => env('MAIL_MAILER', 'smtp'),
 
+
     /*
     |--------------------------------------------------------------------------
     | Mailer Configurations
@@ -69,6 +70,14 @@ return [
 
         'array' => [
             'transport' => 'array',
+        ],
+    ],
+
+    'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer' => false,
+            'verify_peer_name' => false,
         ],
     ],
 
