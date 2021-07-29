776ac537979517479cd27ed964cbd8cd8a45a895
williamnwogbo <williamnwogbo@gmail.com>
2021-07-29 18:10:47 +0100

    Receipt

--- resources/views/email/receipt.blade.php
+++ resources/views/email/receipt.blade.php
@@ -311,7 +311,7 @@
                                             Hi {{ $booking->first_name }} {{ $booking->last_name }}<br/>
                                             Thank you for choosing to book with us. <br/>
                                             Booking Reference: {{ $code }}.<br/>
-                                            Test Provider: {{ $booking_product->vendor->name }},
+                                            Test Provider: {{ $booking_product->vendor->name }}
                                             <br/>
                                             Please take a note of the above as you will require these to complete your passenger locator form
                                             .<br/>
