579da9cf811fe9c42faafbb6a6a978b91b04bb0b
williamnwogbo <williamnwogbo@gmail.com>
2021-08-03 15:08:22 +0100

    success page update

--- resources/views/homepage/success.blade.php
+++ resources/views/homepage/success.blade.php
@@ -26,9 +26,9 @@
                         @if(isset($booking))
                             <h3>Hi, {{ $booking->first_name }} {{ $booking->last_name }}</h3>
                             <p style="font-size:20px;color:#5C5C5C;">Thank you for booking with us, Here is your
-                                code {{ $booking->booking_code }}, An email has also been sent to you. Be sure to also check your junk mail also.
-                                To fill your passenger location form<a href="https://provide-journey-contact-details.homeoffice.gov.uk/passengerLocatorFormUserAccountHolderQuestion"
-                                               target="_blank">Click here.</a>
+                                code {{ $booking->booking_code }}, An email has also been sent to you. Be sure to also check your junk mail also.<br/>
+                                To fill your passenger location form <a href="https://provide-journey-contact-details.homeoffice.gov.uk/passengerLocatorFormUserAccountHolderQuestion"
+                                               target="_blank" style="color: blue">Click here.</a>
                                 @if($booking->referral_code)
                                     <br/> <br/> <a href="{{ url('/booking?ref='.$booking->referral_code) }}"
                                                    class="btn btn-primary">Make another booking</a>
