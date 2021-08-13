a7797b38e4c545630dce5935bc218e996b92a0c7
emperor <edimabassey2@gmail.com>
2021-08-13 13:58:37 +0100

    some checks on migration

--- database/migrations/2021_08_09_164354_add_extra_fields_to_users_table.php
+++ database/migrations/2021_08_09_164354_add_extra_fields_to_users_table.php
@@ -15,11 +15,13 @@ class AddExtraFieldsToUsersTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
-            $table->string('director', 500)->nullable()->after('agent_show_name');
-            $table->string('c_o_i', 500)->nullable()->after('agent_show_name');
-            $table->string('certified', 500)->nullable()->after('agent_show_name');
-            $table->string('certified_no', 500)->nullable()->after('agent_show_name');
-            $table->string('platform_name', 500)->nullable()->after('agent_show_name');
+            if(!Schema::hasColumn('users', 'director') && !Schema::hasColumn('users', 'c_o_i') && !Schema::hasColumn('users', 'certified') && !Schema::hasColumn('users', 'certified_no') && !Schema::hasColumn('users', 'platform_name')){
+                $table->string('director', 500)->nullable()->after('agent_show_name');
+                $table->string('c_o_i', 500)->nullable()->after('agent_show_name');
+                $table->string('certified', 500)->nullable()->after('agent_show_name');
+                $table->string('certified_no', 500)->nullable()->after('agent_show_name');
+                $table->string('platform_name', 500)->nullable()->after('agent_show_name');
+            }
         });
     }
 
@@ -32,6 +34,13 @@ class AddExtraFieldsToUsersTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
+            if(Schema::hasColumn('users', 'director') && Schema::hasColumn('users', 'c_o_i') && Schema::hasColumn('users', 'certified') && Schema::hasColumn('users', 'certified_no') && Schema::hasColumn('users', 'platform_name')){
+                $table->dropColum('director');
+                $table->dropColum('c_o_i');
+                $table->dropColum('certified');
+                $table->dropColum('certified_no');
+                $table->dropColum('platform_name');
+            }
         });
     }
 }
