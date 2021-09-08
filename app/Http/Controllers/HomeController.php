703ea17f463e00ed2b7e14be94221b38577f8868
emperor <edimabassey2@gmail.com>
2021-09-08 11:26:08 +0100

    test phase 2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -348,7 +348,7 @@ class HomeController extends Controller
         curl_close($ch);
 
         $data_response = json_decode($response);
-dd($data_response);
+
         if (isset($data_response->data->status) && $data_response->data->status == "successful") {
 
             $booking = Booking::where('transaction_ref', $txRef)->first();
