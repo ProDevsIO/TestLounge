51ed7bf290f26a1c47311c8f57da70c768767f56
emperor <edimabassey2@gmail.com>
2021-09-08 11:38:26 +0100

    test phase 3

--- resources/views/email/receipt.blade.php
+++ resources/views/email/receipt.blade.php
@@ -311,9 +311,9 @@
                                             Hi {{ $booking->first_name }} {{ $booking->last_name }}<br/>
                                             Thank you for choosing to book with us. <br/>
                                             Booking Reference: 
-                                            @foreach($code as $decode)
-                                            {{ $decode }}
-                                            @endforeach.<br/>
+                                            
+                                            {{ code }}
+                                            <br/>
                                             Test Provider: {{ $booking_product->vendor->name }}
                                             <br/>
                                             Please take a note of the above as you will require these to complete your passenger locator form
