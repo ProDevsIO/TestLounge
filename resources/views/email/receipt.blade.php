8b8b3b8b72f3c4db85faceb95eb8afedeb96e706
emperor <edimabassey2@gmail.com>
2021-09-08 11:43:57 +0100

    test phase 4

--- resources/views/email/receipt.blade.php
+++ resources/views/email/receipt.blade.php
@@ -312,7 +312,7 @@
                                             Thank you for choosing to book with us. <br/>
                                             Booking Reference: 
                                             
-                                            {{ code }}
+                                            {{ $code }}
                                             <br/>
                                             Test Provider: {{ $booking_product->vendor->name }}
                                             <br/>
