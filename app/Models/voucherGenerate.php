5217a1e552172c87c1555829092760bf4cf88c5b
emperor <edimabassey2@gmail.com>
2021-09-10 07:26:19 +0100

    Voucher test phase 4

--- app/Models/voucherGenerate.php
+++ app/Models/voucherGenerate.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class voucherGenerate extends Model
+class VoucherGenerate extends Model
 {
     use HasFactory;
 
