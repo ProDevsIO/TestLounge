e9c0156b156f237513e90ae4db341b0839faf5a7
emperor <edimabassey2@gmail.com>
2021-11-05 10:29:01 +0100

    calculation reverted for revenue

--- resources/views/admin/report.blade.php
+++ resources/views/admin/report.blade.php
@@ -74,7 +74,7 @@
                                         <i class="vl_book"></i>
                                     </div>
                                     <div class="media-body text-light" title="Total Revenue in dollars from bookings">
-                                        <h4 class="text-uppercase mb-0 weight500">${{ number_format($total_revenue_p, 2) }}</h4>
+                                        <h4 class="text-uppercase mb-0 weight500">${{ number_format($profit_dollars, 2) }}</h4>
                                         <span>Total Revenue(Dollars)</span>
                                     </div>
                                 </div>
@@ -91,7 +91,7 @@
                                         <i class="vl_book"></i>
                                     </div>
                                     <div class="media-body text-light" title="Total Vendor Cost">
-                                        <h4 class="text-uppercase mb-0 weight500">₦{{ number_format($vendor_cost_ngn, 2) }}</h4>
+                                        <h4 class="text-uppercase mb-0 weight500">₦{{ number_format($profit_naira, 2) }}</h4>
                                         <span>Total Vendor Cost(Naira)</span>
                                     </div>
                                 </div>
