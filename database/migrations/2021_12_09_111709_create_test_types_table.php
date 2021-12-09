413d6575cfea6ca7ffc43336a70dd16479f39acc
emperor <edimabassey2@gmail.com>
2021-12-09 13:46:26 +0100

    isolation country list expanded

---
+++ database/migrations/2021_12_09_111709_create_test_types_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test_types', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->string('test_type', 200); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('test_types');
+    }
+}
